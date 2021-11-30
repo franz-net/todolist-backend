62affaaa3626a9746e0a38e5b0e924c85ab2e287
Franz <5334409+franz-net@users.noreply.github.com>
2021-11-30 13:00:49 -0600

    added serialization using marshmallow

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify, request
-from models.task import Task
+from models.task import Task, TaskSchema
 
 app = Flask(__name__)
 
 tasks = [
-    Task("Thrash day","On Wednesdays take the trhash out",False),
-    Task("Chorus","Take sofia to chorus",True),
+    Task("Thrash day", "On Wednesdays take the trhash out", "Completed"),
+    Task("Chorus", "Take sofia to chorus", "In Progress"),
 ]
 
 
 @app.route("/api/v1/task")
 def getTasks():
-    return jsonify(tasks)
+    schema = TaskSchema(many=True)
+
+    return jsonify(schema.dump(tasks))
--- models/task.py
+++ models/task.py
@@ -1,8 +1,17 @@
+from marshmallow import Schema, fields
+
+
 class Task:
-  def __init__(self, title, description, status):
-    self.title = title
-    self.description = description
-    self.status = status
-  
-  def describe_task(self):
-    print(f'title: {self.title} task: {self.description} status: {self.status}')
+    def __init__(self, title, description, status):
+        self.title = title
+        self.description = description
+        self.status = status
+
+    def __repr__(self):
+        return "<Task(name={self.title!r}>".format(self=self)
+
+
+class TaskSchema(Schema):
+    title = fields.Str()
+    description = fields.Str()
+    status = fields.Str()
